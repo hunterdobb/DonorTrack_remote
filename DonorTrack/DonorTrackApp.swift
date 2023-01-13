e514ac385d5233a48226ae7b93bfc8daaee89e82
Hunter Dobbelmann <hunterdobb@gmail.com>
2023-01-13 16:22:49 -0500

    Initial Commit

---
+++ DonorTrack/DonorTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonorTrackApp.swift
+//  DonorTrack
+//
+//  Created by Hunter Dobbelmann on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct DonorTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
